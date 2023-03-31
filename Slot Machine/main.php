ee30c0040dd280f3cc5e6cc4faefb09dcdf113ab
Vitalijs Mikutis <vitalijsmikutis@gmail.com>
2023-03-31 14:19:48 +0300

    first commit

---
+++ Slot Machine/main.php
@@ -0,0 +1,88 @@
+<?php
+
+$board = [[]];
+
+function addGameEleements(string $name, int $price)
+{
+    $gameSymbols = new stdClass();
+    $gameSymbols->name = $name;
+    $gameSymbols->price = $price;
+    return $gameSymbols;
+}
+
+$gameSymbols = [
+    0 => addGameEleements("A", 5),
+    1 => addGameEleements("K", 4),
+    2 => addGameEleements("Q", 3),
+    3 => addGameEleements("J", 2),
+    4 => addGameEleements("10", 1)
+];
+
+$winningLines = [
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    [[2, 0], [1, 1], [0, 2]]
+];
+
+$deposit = readline("Add your cash! ");
+
+$playAgain = true;
+
+while ($playAgain) {
+    if ($deposit <= 0) {
+        echo "You're out of money.\n";
+        break;
+    }
+
+    // create random elements on board
+    for ($row = 0; $row < 3; $row++) {
+        for ($i = 0; $i < 3; $i++) {
+            $board[$row][$i] = $gameSymbols[rand(0, 4)];
+        }
+    }
+
+    echo "Your deposit is " . $deposit . "\n";
+
+    $spin = readline("Spin? (y/n) ");
+
+    if ($spin === "y") {
+        $playAgain = true;
+    } else {
+        $playAgain = false;
+    }
+
+    $deposit -= 1;
+
+    // display board
+    foreach ($board as $row) {
+        foreach ($row as $element) {
+            echo " - ";
+            echo $element->name . " ";
+            echo " - ";
+        }
+        echo "\n";
+    }
+
+    $lineOfVal = [];
+
+    foreach ($winningLines as $line) {
+
+        foreach ($line as $coord) {
+            $row = $coord[0];
+            $col = $coord[1];
+            $lineOfVal[] = $board[$row][$col];
+        }
+    }
+
+    $lines = array_chunk($lineOfVal, 3);
+
+    for ($i = 0; $i < count($lines); $i++) {
+        if (count(array_unique(array_column($lines[$i], 'name'))) === 1) {
+            echo "We got a line! of symbol(s): " . $lines[$i][0]->name;
+            echo PHP_EOL;
+            $deposit += (int)$lines[$i][0]->price;
+        }
+    }
+}
---
+++ VendingMachine/main.php
@@ -0,0 +1,64 @@
+<?php
+
+function addProducts(string $name, int $price)
+{
+    $products = new stdClass();
+    $products->name = $name;
+    $products->price = $price;
+    return $products;
+}
+
+$coins = [
+    200, 100, 50, 20, 10, 5, 2, 1
+];
+
+$products = [
+    1 => addProducts("Coffee (XL)", 280),
+    2 => addProducts("Coffee (L)", 190),
+    3 => addProducts("Hot Chocolate", 175),
+    4 => addProducts("Tea", 175)
+];
+
+foreach ($products as $key => $product) {
+    echo $key . " " . $product->name . "|   Price: " . $product->price;
+    echo PHP_EOL;
+}
+
+
+$selection = strtolower(readline("Please select your drink!(1-4) "));
+
+$deposit = 0;
+
+while ($deposit < $products[$selection]->price) {
+    echo "Your selection is: " . $products[$selection]->name . " Price:" . $products[$selection]->price;
+    echo PHP_EOL;
+
+    echo "Your deposid is: " . $deposit;
+    echo PHP_EOL;
+    $insertedCoins = readline("Enter your coin: ");
+
+
+    if (!in_array($insertedCoins, $coins)) {
+        echo "invalid coin";
+        echo PHP_EOL;
+    } else {
+        $deposit += $insertedCoins;
+    }
+
+}
+
+echo PHP_EOL;
+
+echo "Your total deposit was {$deposit} and " . "Reminder is: " . $reminder = $deposit - ($products[$selection]->price);
+echo PHP_EOL;
+echo "Take your change: ";
+foreach ($coins as $coin) {
+    $times = floor($reminder / $coin,);
+    if ($times > 0) {
+        echo "Coin:" . $coin . " Times " . $times;
+        echo PHP_EOL;
+    }
+
+    $reminder -= $coin * $times;
+
+}
